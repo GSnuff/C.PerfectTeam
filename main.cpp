c0441a2241c8d032a2a502cc467392abdd7a322a
Kutuzov Vlad <alphagrimsaw@gmail.com>
2020-03-15 21:03:48 +0200

    Likely the clearest one for now

--- main.cpp
+++ main.cpp
@@ -1,70 +1,56 @@
 #include <vector>
 #include <iostream>
 
-size_t max(const size_t &first, const size_t &second);
-size_t min(const size_t &first, const size_t &second);
-size_t maxPerfectTeams(const size_t &nMath, const size_t &nCoders, const size_t &nUndefined);
+uint32_t max(const uint32_t &first, const uint32_t &second);
+uint32_t min(const uint32_t &first, const uint32_t &second);
+uint32_t maxPerfectTeams(const uint32_t &nMath, const uint32_t &nCoders, const uint32_t &nUndefined);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
-    size_t nRequest;
-    size_t nMath, nCoders, nUndef;
+    uint32_t nRequest;
+    uint32_t nMath, nCoders, nUndef;
 
     std::cin >> nRequest;
-    std::vector<size_t> perfectTeams;
+    std::vector<uint32_t > perfectTeams;
 
-    for (size_t i = 0; i < nRequest; ++i) {
+    for (uint32_t i = 0; i < nRequest; ++i) {
         std::cin >> nMath >> nCoders >> nUndef;
         perfectTeams.push_back(maxPerfectTeams(nMath, nCoders, nUndef));
     }
 
-    for (size_t perfectTeam : perfectTeams)
+    for (uint32_t perfectTeam : perfectTeams)
         std::cout << perfectTeam << ' ' ;
 
 
 }
 
-size_t max(const size_t &first, const size_t &second) {
+uint32_t max(const uint32_t &first, const uint32_t &second) {
     return (first > second ? first : second);
 }
 
-size_t min(const size_t &first, const size_t &second) {
+uint32_t min(const uint32_t &first, const uint32_t &second) {
     return (first < second ? first : second);
 }
 
-void detectRange(int &left, int &right) {
+uint32_t maxPerfectTeams(const uint32_t &nMath, const uint32_t &nCoders, const uint32_t &nUndefined) {
+    const uint32_t maxOfMathAndCoders = max(nMath, nCoders);
+    const uint32_t maxTeams = min(nMath, nCoders);
 
-}
-
-size_t maxPerfectTeams(const size_t &nMath, const size_t &nCoders, const size_t &nUndefined) {
-    const int maximum = max(nMath, nCoders);
-    int maxTeams = min(nMath, nCoders);
-    const int max_teams = min(nMath, nCoders);
-    int undefined = nUndefined;
-    int prev = 0;
-
-    while (maxTeams != 0 && ((maximum + undefined) / maxTeams) < 2) {
-        prev = maxTeams;
-        maxTeams /= 2;
+    // detecting range
+    uint32_t L = maxTeams, R=maxTeams;
+    while (L != 0 && ((maxOfMathAndCoders + nUndefined) / L) < 2) {
+        R = L;
+        L /= 2;
     }
 
-    if(maxTeams == 0)
+    if(R == 0)
         return 0;
 
-    int L = maxTeams;
-    int R = prev;
-    if (L == R)
-        return L;
-
-    int mid;
-    --L;
-    ++R;
-    int res;
     while (R - L > 1) {
-        mid = (L + R) / 2;
-        res = (maximum + undefined + (max_teams - mid)) / mid;
+        uint32_t mid = (L + R) / 2;
+        uint32_t res = (maxOfMathAndCoders + nUndefined + (maxTeams - mid)) / mid;
         if (res >= 2) {
             L = mid;
         } else {
