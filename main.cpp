e2e7f7c7d3d10eb8969843434c10e05022206ee3
Kutuzov Vlad <alphagrimsaw@gmail.com>
2020-03-15 20:23:36 +0200

    Deleted L==R check

--- main.cpp
+++ main.cpp
@@ -1,22 +1,9 @@
-#include <algorithm>
-#include <cmath>
 #include <vector>
 #include <iostream>
 
-#define nREQUESTS_MAX 1e4
-#define nPARTICIPANT_MAX 1e8
-
-size_t maxPerfectTeams(size_t &nMath, size_t &nCoders, size_t &nUndefined);
-
-
-size_t max(const size_t &first, const size_t &second) {
-    return (first > second ? first : second);
-}
-
-size_t min(const size_t &first, const size_t &second) {
-    return (first < second ? first : second);
-}
-
+size_t max(const size_t &first, const size_t &second);
+size_t min(const size_t &first, const size_t &second);
+size_t maxPerfectTeams(const size_t &nMath, const size_t &nCoders, const size_t &nUndefined);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
@@ -24,7 +11,6 @@ int main() {
 
     size_t nRequest;
     size_t nMath, nCoders, nUndef;
-
 
     std::cin >> nRequest;
     std::vector<size_t> perfectTeams;
@@ -35,80 +21,53 @@ int main() {
     }
 
     for (size_t perfectTeam : perfectTeams)
-        std::cout << ' ' << perfectTeam;
+        std::cout << perfectTeam << ' ' ;
 
 
 }
 
-class BinarySearch {
-public:
-    BinarySearch(size_t math, size_t coders, size_t undefined);
-
-    size_t binarySearch(const size_t &L, const size_t &R);
-
-private:
-    size_t nMath{0};
-    size_t nCoders{0};
-    size_t nUndef{0};
-
-    bool check(const size_t &numerator, const size_t &denom);
-
-    std::vector<size_t> vectorInit(const size_t L, const size_t R);
-
-
-    size_t &max(size_t &first, size_t &second);
-
-    size_t &min(size_t &first, size_t &second);
-
-};
-
-BinarySearch::BinarySearch(const size_t math, const size_t coders, const size_t undefined)
-        : nMath(math), nCoders(coders), nUndef(undefined) {}
-
-
-size_t BinarySearch::binarySearch(const size_t &L, const size_t &R) {
-    return 0;
-}
-
-size_t &BinarySearch::max(size_t &first, size_t &second) {
+size_t max(const size_t &first, const size_t &second) {
     return (first > second ? first : second);
 }
 
-size_t &BinarySearch::min(size_t &first, size_t &second) {
+size_t min(const size_t &first, const size_t &second) {
     return (first < second ? first : second);
 }
 
-bool BinarySearch::check(const size_t &numerator, const size_t &denom) {
-    return (numerator / denom) > 2;
-}
+size_t maxPerfectTeams(const size_t &nMath, const size_t &nCoders, const size_t &nUndefined) {
+    int maximum = max(nMath, nCoders);
+    int maxTeams = min(nMath, nCoders);
+    int mn2 = min(nMath, nCoders);
+    int undefined = nUndefined;
+    int L;
+    int R = min;
 
-std::vector<size_t> BinarySearch::vectorInit(const size_t L, const size_t R) {
-    const size_t N = R - L;
-    std::vector<size_t> v(N);
-    size_t i = L;
-    for (auto p = v.begin();  p < v.end(); ++p, ++i) {
-        v[*p] = i;
+    while (maxTeams != 0 && ((maximum + undefined) / maxTeams) < 2) {
+        R = maxTeams;
+        maxTeams /= 2;
     }
 
-}
+    if(maxTeams == 0)
+        return 0;
 
+    L = maxTeams;
 
-size_t maxPerfectTeams(size_t &nMath, size_t &nCoders, size_t &nUndefined) {
-    size_t mx = max(nMath, nCoders);
-    size_t mn = min(nMath, nCoders);
-    size_t others = nUndefined;
-    size_t L = mn;
-    size_t R = mn;
+    if (L == R)
+        return L;
 
-    if( ((mx + others) / mn) > 2 ) {
-        R = mn;
-        mn /= 2;
-    } else {
-        L = mn;
+    int mid;
+    --L;
+    ++R;
+    int res;
+    while (R - L > 1) {
+        mid = (L + R) / 2;
+        res = (maximum + undefined + (mn2 - mid)) / mid;
+        if (res >= 2) {
+            L = mid;
+        } else {
+            R = mid;
+        }
     }
-
-
-    return 0;
-
+    return L;
 }
 
